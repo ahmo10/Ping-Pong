4d00a2f83a81c29eba6bc1feb23e9e4eaeca2f58
Ahmed <ahmey.ibra@gmail.com>
2018-07-15 14:04:48 +0300

    added js functionality

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function enterNumber(number) {
+
+  while (isNaN(parseInt(number))) {
+    number= parseInt(prompt("Please enter a number: "));
+  }
+
+  for (var count = 1; count<= number; count++) {
+    
+    if (count%3===0 && count%5===0) {
+      document.write("PingPong"+ "<br>");
+      continue;
+    }
+     else if (count%3===0) {
+      document.write("Ping" + "<br>");
+      continue;
+    }
+    else if (count%5===0 ) {
+      document.write("Pong" + "<br>");
+      continue;
+    }  else {
+        document.write(count + "<br>");
+        continue;
+      }
+    }
+  }
