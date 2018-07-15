9429fd9e8bb48c4503e213572ccad250e873f194
Ahmed <ahmey.ibra@gmail.com>
2018-07-15 23:29:22 +0300

    added about pingpong details

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,37 @@
-function enterNumber(number){
+function enterNumber(number) {
   var userInput = document.getElementById('number').value
-  var results = document.getElementById('results')
-  var listElement = document.createElement('ul')
-  results.appendChild(listElement)
-  var inputValue = parseInt(userInput)
+  var results = document.getElementById('results');
+  var listElement = document.createElement('ul');
+  results.appendChild(listElement);
+  var inputValue = parseInt(userInput);
   for (var count = 1; count <= inputValue; count++) {
     var listItem = document.createElement('li');
-    listItem.innerHTML = divisiblebyThree(count);
-    listElement.appendChild(listItem)
+    listItem.innerHTML = divisiblebyFifteen(count);
+    listElement.appendChild(listItem);
   }
 }
 
-  function divisiblebyThree(number) {
-    if (number % 3 === 0) {
-      return "ping"
+function divisiblebyFifteen(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "pingpong";
 
-    }
-    return divisiblebyFifteen(number);
   }
-  function divisiblebyFifteen(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-      return "pingpong"
+  return divisiblebyThree(number)
+}
 
-    }
-    return divisiblebyFive(number)
+function divisiblebyThree(number) {
+  if (number % 3 === 0) {
+    return "ping";
+
   }
+  return divisiblebyFive(number);
+}
 
-    function divisiblebyFive(number) {
-      if (number % 5 === 0) {
-        return "pong"
 
-      }
-      return number;
-    }
+function divisiblebyFive(number) {
+  if (number % 5 === 0) {
+    return "pong";
+
+  }
+  return number;
+}
